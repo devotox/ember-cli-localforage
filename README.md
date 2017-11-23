d055fa10ca7eff534f2819539417b3fe6da2f3f7
Devonte <devo.tox.89@gmail.com>
2017-11-23 23:26:03 +0000

    Fix tests

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 [![Ember Observer Score](http://emberobserver.com/badges/ember-cli-localforage.svg)](http://emberobserver.com/addons/ember-cli-localforage)
 [![Build Status](https://travis-ci.org/devotox/ember-cli-localforage.svg)](http://travis-ci.org/devotox/ember-cli-localforage)
-[![Coverage Status](https://coveralls.io/repos/github/devotox/mber-localforage/badge.svg)](https://coveralls.io/github/devotox/ember-cli-localforage)
+[![Coverage Status](https://codecov.io/gh/devotox/ember-cli-localforage/branch/master/graph/badge.svg)](https://codecov.io/gh/devotox/ember-cli-localforage)
 [![NPM Version](https://badge.fury.io/js/ember-cli-localforage.svg)](http://badge.fury.io/js/ember-cli-localforage)
 [![NPM Downloads](https://img.shields.io/npm/dm/ember-cli-localforage.svg)](https://www.npmjs.org/package/ember-cli-localforage)
+[![Dependency Status](https://david-dm.org/poetic/ember-cli-localforage.svg)](https://david-dm.org/poetic/ember-cli-localforage)
+[![DevDependency Status](https://david-dm.org/poetic/ember-cli-localforage/dev-status.svg)](https://david-dm.org/poetic/ember-cli-localforage#info=devDependencies)
 [![Greenkeeper](https://badges.greenkeeper.io/devotox/ember-cli-localforage.svg)](https://greenkeeper.io/)
+
 
 ## Description
 Simple wrapper around localforage to allow usage in ember applications
--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 /* eslint-env node */
+'use strict';
+
 var FastbootTransform = require('fastboot-transform');
 
 module.exports = {
-  name: 'ember-cli-localforage',
-  options: {
-  nodeAssets: {
-    localforage: {
-      vendor: {
-          srcDir: 'dist',
-          include: ['localforage.js'],
-          processTree(input) {
-          return FastbootTransform(input);
-        }
-      }
-    }
-  }
-  },
-  included() {
-    this._super.included.apply(this, arguments);
-    this._ensureThisImport();
+	name: 'ember-cli-localforage',
+	options: {
+		nodeAssets: {
+			localforage: {
+				vendor: {
+					srcDir: 'dist',
+					include: ['localforage.js'],
+					processTree(input) {
+						return FastbootTransform(input);
+					}
+				}
+			}
+		}
+	},
+	included() {
+		this._super.included.apply(this, arguments);
+		this._ensureThisImport();
 
-    this.import('vendor/localforage/localforage.js');
-    this.import('vendor/shims/localforage.js');
-  },
-  _ensureThisImport() {
-    if (!this.import) {
-      this._findHost = function findHostShim() {
-        let current = this;
-        let app;
+		this.import('vendor/localforage/localforage.js');
+		this.import('vendor/shims/localforage.js');
+	},
+	_ensureThisImport() {
+		if (!this.import) {
+			this._findHost = function findHostShim() {
+				let current = this;
+				let app;
 
-        do {
-          app = current.app || app;
-        } while (current.parent.parent && (current = current.parent));
-        return app;
-      };
-      this.import = function importShim(asset, options) {
-        let app = this._findHost();
-        app.import(asset, options);
-      };
-    }
-  }
+				do {
+					app = current.app || app;
+				} while (current.parent.parent && (current = current.parent));
+				return app;
+			};
+			this.import = function importShim(asset, options) {
+				let app = this._findHost();
+				app.import(asset, options);
+			};
+		}
+	}
 };
-
-
